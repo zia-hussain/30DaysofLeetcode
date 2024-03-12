42255dbfa9df9cbc8d05e795208c5ca44fdf1657
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-03-13 01:33:51 +0500

    Time: 84 ms (17.31%), Space: 59.5 MB (25.30%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -4,6 +4,10 @@
  */
 var maxProfit = function(prices) {
   let minPrice = Infinity, maxProfit = 0;
-  for (let price of prices) minPrice = Math.min(minPrice, price), maxProfit = Math.max(maxProfit, price - minPrice);
+  for (let i = 0; i < prices.length; i++) {
+    let price = prices[i];
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
+  }
   return maxProfit;
 }
