91f72ba68b12128fc6aa4902403b86ef91a8e9c3
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-03-13 19:42:15 +0500

    Time: 49 ms (92.22%), Space: 49.5 MB (67.11%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -3,19 +3,22 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-      if (!nums || nums.length === 0) return 0;
-  let maxProductSoFar = nums[0];
-  let minProductSoFar = nums[0];
-  let result = nums[0];
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] < 0) {
-      let temp = maxProductSoFar;
-      maxProductSoFar = minProductSoFar;
-      minProductSoFar = temp;
+    if (!nums || nums.length === 0) return 0;
+  
+    let maxProductSoFar = nums[0];
+    let minProductSoFar = nums[0];
+    let result = nums[0];
+  
+    for (let i = 1; i < nums.length; i++) {
+        const current = nums[i];
+        const tempMax = maxProductSoFar * current;
+        const tempMin = minProductSoFar * current;
+  
+        maxProductSoFar = Math.max(current, tempMax, tempMin);
+        minProductSoFar = Math.min(current, tempMax, tempMin);
+  
+        result = Math.max(result, maxProductSoFar);
     }
-    maxProductSoFar = Math.max(nums[i], maxProductSoFar * nums[i]);
-    minProductSoFar = Math.min(nums[i], minProductSoFar * nums[i]);
-    result = Math.max(result, maxProductSoFar);
-  }
-  return result;
+  
+    return result;
 };
