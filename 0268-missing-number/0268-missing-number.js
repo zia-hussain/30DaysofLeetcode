2b9ade0976f6a86e97a90d64110261f171612c82
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-03-13 17:17:22 +0500

    Time: 47 ms (95.59%), Space: 51.3 MB (59.72%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -5,6 +5,6 @@
 var missingNumber = function(nums) {
     let n = nums.length
     let expSum = (n*(n+1)/2)
-    let sum = nums.reduce((acc,cur)=>acc+cur)
+    let sum = nums.reduce((acc,cur)=>acc+cur,0)
     return expSum-sum
 };
