938a081be60c2e079e30d4156afa3a6836e9f80d
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-03-13 19:40:51 +0500

    Time: 64 ms (29.06%), Space: 49.5 MB (67.11%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -4,23 +4,18 @@
  */
 var maxProduct = function(nums) {
       if (!nums || nums.length === 0) return 0;
-
   let maxProductSoFar = nums[0];
   let minProductSoFar = nums[0];
   let result = nums[0];
-
   for (let i = 1; i < nums.length; i++) {
     if (nums[i] < 0) {
       let temp = maxProductSoFar;
       maxProductSoFar = minProductSoFar;
       minProductSoFar = temp;
     }
-
     maxProductSoFar = Math.max(nums[i], maxProductSoFar * nums[i]);
     minProductSoFar = Math.min(nums[i], minProductSoFar * nums[i]);
-
     result = Math.max(result, maxProductSoFar);
   }
-
   return result;
 };
