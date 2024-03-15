f7f604049347f4f835ad1cbd36d73a266cb9b33b
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-03-16 01:40:59 +0500

    Time: 55 ms (39.96%), Space: 48.4 MB (87.62%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -5,9 +5,14 @@
 var findMin = function(nums) {
     let left = 0;
     let right = nums.length - 1;
+
+    if (nums[left] < nums[right]) {
+        return nums[left];
+    }
     
     while (left < right) {
         let mid = Math.floor((left + right) / 2);
+        
         if (nums[mid] > nums[right]) {
             left = mid + 1;
         } else {
