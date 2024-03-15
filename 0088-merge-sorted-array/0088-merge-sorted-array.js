f6324def288bc95a1c94e26db65119d6b0f17ce9
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-03-15 06:43:49 +0500

    Time: 51 ms (69.84%), Space: 49.3 MB (21.07%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -9,7 +9,7 @@ var merge = function(nums1, m, nums2, n) {
  let index1 = m - 1;
     let index2 = n - 1;
     let mergedIndex = m + n - 1;
-
+    
     while (index1 >= 0 && index2 >= 0) {
         if (nums1[index1] > nums2[index2]) {
             nums1[mergedIndex--] = nums1[index1--];
@@ -17,7 +17,6 @@ var merge = function(nums1, m, nums2, n) {
             nums1[mergedIndex--] = nums2[index2--];
         }
     }
-
     while (index2 >= 0) {
         nums1[mergedIndex--] = nums2[index2--];
     }
