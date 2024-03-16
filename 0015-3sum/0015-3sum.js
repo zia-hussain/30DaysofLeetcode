ef60cb76ed6f0c6bffa48c829531af386b0d889b
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-03-17 01:56:24 +0500

    Time: 148 ms (86.77%), Space: 63.9 MB (91.09%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,21 +3,24 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums.sort((a, b) => a - b);
+     nums.sort((a, b) => a - b);
     const result = [];
     
     for (let i = 0; i < nums.length - 2; i++) {
         if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
-            let low = i + 1, high = nums.length - 1, sum = 0 - nums[i];
+            let low = i + 1, high = nums.length - 1;
+            const target = -nums[i];
             
             while (low < high) {
-                if (nums[low] + nums[high] === sum) {
+                const sum = nums[low] + nums[high];
+                
+                if (sum === target) {
                     result.push([nums[i], nums[low], nums[high]]);
                     while (low < high && nums[low] === nums[low + 1]) low++;
                     while (low < high && nums[high] === nums[high - 1]) high--;
                     low++;
                     high--;
-                } else if (nums[low] + nums[high] < sum) {
+                } else if (sum < target) {
                     low++;
                 } else {
                     high--;
@@ -27,4 +30,5 @@ var threeSum = function(nums) {
     }
     
     return result;
+
 };
