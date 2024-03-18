074b64c770bdec3bb19c27aae80286fc25a33fa9
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-03-18 06:52:56 +0500

    Time: 135 ms (13.71%), Space: 58.6 MB (96.62%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,5 +3,9 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    return new Set(nums).size !== nums.length;
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) return true;
+    }
+    return false;
 };
