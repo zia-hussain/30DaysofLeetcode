7b02361001e1fb2a55d3ee1728281bf65ec2dd7f
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-03-18 07:04:05 +0500

    Time: 63 ms (27.66%), Space: 49.9 MB (60.70%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -9,9 +9,8 @@ var generate = function(numRows) {
         const row = [];
         for (let j = 0; j <= i; j++) {
             if (j === 0 || j === i) {
-                row.push(1); // First and last elements of each row are always 1
+                row.push(1);
             } else {
-                // Calculate element using binomial coefficient formula
                 row.push(triangle[i - 1][j - 1] + triangle[i - 1][j]);
             }
         }
