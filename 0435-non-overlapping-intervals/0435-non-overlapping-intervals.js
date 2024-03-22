b10f292c94ac1c47664440598f45b255bd9e8533
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-03-22 06:12:15 +0500

    Time: 197 ms (68.71%), Space: 79.2 MB (89.22%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -5,20 +5,16 @@
 var eraseOverlapIntervals = function(intervals) {
        if (intervals.length === 0) return 0;
     intervals.sort((a, b) => a[1] - b[1]);
-
     let count = 0;
     let prevEnd = intervals[0][1];
-
     for (let i = 1; i < intervals.length; i++) {
         const currStart = intervals[i][0];
         const currEnd = intervals[i][1];
-
         if (currStart < prevEnd) {
             count++;
         } else {
             prevEnd = currEnd;
         }
     }
-
     return count;
 };
