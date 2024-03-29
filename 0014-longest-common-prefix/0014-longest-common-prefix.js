1c73abb04b653303d68ddb15327cc12fba92dad5
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-03-30 00:23:10 +0500

    Time: 54 ms (66.02%), Space: 48.7 MB (91.02%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -4,9 +4,11 @@
  */
 var longestCommonPrefix = function(strs) {
      if (strs.length === 0) return "";
-    for (let i = 0; i < strs[0].length; i++) {
-        for (let j = 1; j < strs.length; j++) {
-            if (strs[j][i] !== strs[0][i] || i === strs[j].length) {
-                return strs[0].substring(0, i)}}}
-    return strs[0];
+  return strs.reduce((prefix, str) => {
+        let i = 0;
+        while (i < prefix.length && i < str.length && prefix[i] === str[i]) {
+            i++;
+        }
+        return prefix.slice(0, i);
+    }, strs[0]);
 };
