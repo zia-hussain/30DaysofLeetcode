8cb92feab2d73674b6ffa7403c363e4b38dfbf6d
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-03-31 06:21:34 +0500

    Time: 59 ms (80.21%), Space: 53.1 MB (8.07%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,12 +4,10 @@
  */
 var removeDuplicates = function(nums) {
     if (nums.length === 0) return 0;
-    let index = 0;
-
+    let count = 1; // Count of unique elements
     for (let i = 1; i < nums.length; i++) {
-        if (nums[i] !== nums[index]) {
-            index++;
-            nums[index] = nums[i]
+        if (nums[i] !== nums[i - 1]) {
+            nums[count++] = nums[i];
         }}
-    return index + 1;
+    return count;
 };
