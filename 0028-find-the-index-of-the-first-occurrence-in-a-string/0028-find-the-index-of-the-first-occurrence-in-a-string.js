55e3fcc843eedb0f3e7c4d5204b20f651c6c30ea
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-04-01 19:34:54 +0500

    Time: 53 ms (54.25%), Space: 49.4 MB (18.38%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,13 +4,23 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    if (!needle) return 0;
-    for (let i = 0; i <= haystack.length - needle.length; i++) {
-        let j = 0;
-        while (j < needle.length && haystack[i + j] === needle[j]) {
-            j++;
-        }
-        if (j === needle.length) return i;
+    const n = haystack.length;
+    const m = needle.length;
+    if (m === 0) return 0;
+    const last = new Array(256).fill(-1);
+    for (let i = 0; i < m; i++) {
+        last[needle.charCodeAt(i)] = i;
     }
+    let i = m - 1;
+    let j = m - 1;
+    while (i < n) {
+        if (haystack[i] === needle[j]) {
+            if (j === 0) return i;
+            i--;
+            j--;
+        } else {
+            i += m - Math.min(j, 1 + last[haystack.charCodeAt(i)]);
+            j = m - 1;
+        }}
     return -1;
 };
