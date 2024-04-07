274ff7105b52743675477ac5eb03137764936dfb
Syed Zia Hussain Shah <130322420+zia-hussain@users.noreply.github.com>
2024-04-07 21:01:46 +0500

    Time: 55 ms (86.66%), Space: 53 MB (78.91%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+    const helper = (node, min, max) => {
+        if (!node) return true;
+        if (node.val <= min || node.val >= max) return false;
+        return helper(node.left, min, node.val) && helper(node.right, node.val, max)}
+    return helper(root, -Infinity, Infinity);
+};
